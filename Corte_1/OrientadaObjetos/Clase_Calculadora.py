b6fb037a1670cc4150bfee179aadbdc9f9e75ad7
DanAgu473 <dsagudelor@udistrital.edu.co>
2025-10-23 14:35:01 -0500

    Clase calculadora Agregada

---
+++ Corte_1/OrientadaObjetos/Clase_Calculadora.py
@@ -0,0 +1,28 @@
+class Cuadrado:
+    def __init__(self,val):
+        self.val=val
+    def getVal(self):
+        return self.val*self.val
+
+class Sum_Rest:
+    def sum(self,a,b):
+        return a+b
+    def rest(self,a,b):
+        return a-b
+    
+class Potencia::
+def potencia(self,a,b):
+        return a**b
+
+class Raiz:
+     def raiz_cuadrada(self,a):
+        return a**0.5
+     
+class factorial:
+    def fact(self,a):
+        if a==0 or a==1:
+            return 1
+        else:
+            return a*self.fact(a-1)
+
+
